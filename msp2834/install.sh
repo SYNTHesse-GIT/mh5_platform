3a7d3fad4be62378d8ccb208d3f7646d7819f6dd
sonelu <alex.sonea@gmail.com>
2025-07-09 17:00:47 +0100

    msp2834 added the overaly for touch screen

--- msp2834/install.sh
+++ msp2834/install.sh
@@ -1,3 +1,6 @@
 ./mipi-dbi-cmd ili9341v.bin ili9341v.txt
 sudo cp ili9341v.bin /lib/firmware/
 ./mipi-dbi-cmd /lib/firmware/ili9341v.bin
+
+dtc -I dts -O dtb -o ft6336g.dtbo ft6336g.dts
+sudo cp ft6336g.dtbo /boot/firmware/overlays/
